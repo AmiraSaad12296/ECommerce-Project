805236bc16beb935994a785d8f3fe7a752873f43
Amira <amira.mohamd791@gmail.com>
2024-04-30 21:04:08 +0300

    confirmed

--- E-Commerce Project.PL/E-Commerce.DAL/Models/EcommerceProjectContext.cs
+++ E-Commerce Project.PL/E-Commerce.DAL/Models/EcommerceProjectContext.cs
@@ -43,7 +43,7 @@ public partial class EcommerceProjectContext : DbContext
 
 //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=\"Ecommerce Project\";Integrated Security=True");
+//        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=\"Ecommerce Project\";Integrated Security=True;Encrypt=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -71,11 +71,6 @@ public partial class EcommerceProjectContext : DbContext
             entity.HasOne(d => d.User).WithMany(p => p.Orders)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Order_User");
-        });
-
-        modelBuilder.Entity<Payment>(entity =>
-        {
-            entity.Property(e => e.PaymentId).ValueGeneratedNever();
         });
 
         modelBuilder.Entity<Product>(entity =>
--- E-Commerce Project.PL/E-Commerce.DAL/obj/Debug/net8.0/E-Commerce.DAL.AssemblyInfo.cs
+++ E-Commerce Project.PL/E-Commerce.DAL/obj/Debug/net8.0/E-Commerce.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b7e8f5a976b62e4087496e90ad0476d140a703")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+259ca28d8ecae25fc0c28c74ce781c3ae0eae408")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
